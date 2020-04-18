1cf2061e1ad81812c809c91ef716268ac02ed993
nagarjun.m <nagarjun.m@corp.flock.com>
2020-04-18 11:33:07 +0530

    initial UI structure

---
+++ src/components/appContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Appcontainer() {
+  return (
+    <div className="app-container">
+      <div className="col">col 1</div>
+      <div className="col">col 2</div>
+      <div className="col">col 3</div>
+    </div>
+  );
+}
+
+export { Appcontainer };
---
+++ src/components/workspace/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./workspace.scss";
+
+function Workspace() {
+  return <div className="ws">loading</div>;
+}
